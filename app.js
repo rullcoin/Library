226186d551e69fb7bed20b58095700effcd2fae6
Raul Orban <raulorban1@gmail.com>
2022-07-04 20:00:00 -0400

    refactor with class

--- app.js
+++ app.js
@@ -6,70 +6,73 @@ let form = document.querySelector('.form')
 let formDiv = document.getElementById("form-container")
 let deleteButton = document.getElementById("deleteButton")
 
-
-function Book(title, author, pages, read, id) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.id = id
-}
-
-Book.prototype.changeStatus = function(event) {
-    let eventTargetClass = event.target.classList
-    if (this.read === true) {
-        this.read = false
-        eventTargetClass.replace("change-status-on", "change-status-off")
-        event.target.textContent = "Not read"
-    } else {
-        this.read = true
-        eventTargetClass.replace("change-status-off", "change-status-on")
-        event.target.textContent = "Read"
-    }
-}
-
-Book.prototype.addToDiv = function() {
-    let childDiv = document.createElement("div");
-    childDiv.classList.add("book-card")
-
-    let titleParagraph = document.createElement("h1");
-    let authorParagraph = document.createElement("p");
-    let pagesParagraph = document.createElement("p");
-
-    let buttonContainer = document.createElement("div")
-    buttonContainer.id = "change-status"
-    buttonContainer.classList.add("change-status")
-    
-
-    let deleteButton = document.createElement("button")
-    deleteButton.id = "deleteButton"
-    deleteButton.innerHTML = "Delete"
-
-    let statusButton = document.createElement("button")
-    statusButton.id = "changeStatus"
-    if (this.read) {
-        statusButton.classList.add("change-status-on")
-        statusButton.innerHTML = "Read"
-    } else {
-        statusButton.classList.add("change-status-off")
-        statusButton.innerHTML = "Not read"
+//Test with classes
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
     }
 
-    buttonContainer.append(statusButton, deleteButton)
+    changeStatus(event) {
+        
+        let eventTargetClass = event.target.classList
+        if (this.read === true) {
+            this.read = false
+            eventTargetClass.replace("change-status-on", "change-status-off")
+            event.target.textContent = "Not read"
+        } else {
+            this.read = true
+            eventTargetClass.replace("change-status-off", "change-status-on")
+            event.target.textContent = "Read"
+        }
+    }
 
-    // Needed to add id to button
-    let id = myLibrary.length - 1
-    deleteButton.setAttribute("data-id", id)
+    addToDiv() {
+        let childDiv = document.createElement("div");
+        childDiv.classList.add("book-card")
 
-    titleParagraph.textContent = `${this.title}`
-    authorParagraph.textContent = `By: ${this.author}`
-    pagesParagraph.textContent = `Pages: ${this.pages}`
+        let titleParagraph = document.createElement("h1");
+        let authorParagraph = document.createElement("p");
+        let pagesParagraph = document.createElement("p");
 
-    childDiv.append(titleParagraph, authorParagraph, pagesParagraph, buttonContainer)
-    mainDiv.append(childDiv)
+        let buttonContainer = document.createElement("div")
+        buttonContainer.id = "change-status"
+        buttonContainer.classList.add("change-status")
+        
 
-    deleteButton.addEventListener('click', deleteItem)
-    statusButton.addEventListener("click", this.changeStatus.bind(this))
+        let deleteButton = document.createElement("button")
+        deleteButton.id = "deleteButton"
+        deleteButton.innerHTML = "Delete"
+
+        let statusButton = document.createElement("button")
+        statusButton.id = "changeStatus"
+        if (this.read) {
+            statusButton.classList.add("change-status-on")
+            statusButton.innerHTML = "Read"
+        } else {
+            statusButton.classList.add("change-status-off")
+            statusButton.innerHTML = "Not read"
+        }
+
+        buttonContainer.append(statusButton, deleteButton)
+
+        // Needed to add id to button
+        let id = myLibrary.length - 1
+        deleteButton.setAttribute("data-id", id)
+
+        titleParagraph.textContent = `${this.title}`
+        authorParagraph.textContent = `By: ${this.author}`
+        pagesParagraph.textContent = `Pages: ${this.pages}`
+
+        childDiv.append(titleParagraph, authorParagraph, pagesParagraph, buttonContainer)
+        mainDiv.append(childDiv)
+
+        deleteButton.addEventListener('click', deleteItem)
+        statusButton.addEventListener("click", this.changeStatus.bind(this))
+}
+
 }
 
 /// At the moment, it deletes the wrong id
@@ -77,12 +80,14 @@ function deleteItem(e) {
     let idToDelete = e.target.getAttribute('data-id');
     myLibrary.splice(idToDelete, 1)
     e.target.parentNode.parentNode.remove()
+    console.log(myLibrary);
 }
 
 function addBookToLibrary(bookTitle, bookAuthor, bookPages, bookStatus) {
     userBook = new Book(bookTitle, bookAuthor, bookPages, bookStatus)
     myLibrary.push(userBook)
     displayBook(userBook)
+    console.log(myLibrary);
 }
 
 //Needed function to display book on html when adding new div
